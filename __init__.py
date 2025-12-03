b505a14f317df638f43b1ed7ba05e822cb63bdee
Louis Ouellet <louis_ouellet@hotmail.com>
2025-12-03 14:41:53 -0500

    General: Code Clean-up

--- __init__.py
+++ __init__.py
@@ -9,9 +9,8 @@ from .log import Log
 from .ui import MsgBox, StepIndicator, Form
 
 from .network.diagnostic import Diagnostic
-from .network.wifi import WiFi
 from .network.tools import Tools
 
 __version__ = "1.0.0"
 
-__all__ = ["Application", "CommandLine", "Helper", "Configuration", "Log", "MsgBox", "StepIndicator", "Form", "Diagnostic", "WiFi", "Tools"]
+__all__ = ["Application", "CommandLine", "Helper", "Configuration", "Log", "MsgBox", "StepIndicator", "Form", "Diagnostic", "Tools"]
