ed9c0f11328d51af479ccc3044c4a34e5883f13d
Louis Ouellet <louis_ouellet@hotmail.com>
2025-12-09 14:01:51 -0500

    BUGFIX: Debugging the diagnostic dialog.

--- log.py
+++ log.py
@@ -90,7 +90,7 @@ class Log:
         # Check if verbose logging is enabled and print to console
         if self._configuration.get("log.verbose"):
             for ln in lines:
-                print(f"[{channel}][{level.upper()}][{self._helper.get_now()}] {ln}")
+                print(f"[{self._helper.get_now()}][{level.upper().ljust(9)}][{channel}] {ln}")
 
         # Append lines to the buffer
         with self._lock:
