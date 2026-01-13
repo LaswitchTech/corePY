3d09dadf58ab29def4872a479f64d8f7614156bf
Louis Ouellet <louis_ouellet@hotmail.com>
2026-01-13 12:36:17 -0500

    BUGFIX: Fixing the service configurations

--- service.py
+++ service.py
@@ -54,23 +54,9 @@ class Service:
         # Service configuration defaults
         if self._configuration is not None:
             # How often the main loop wakes up to check due tasks (seconds)
-            self._configuration.add(
-                "service.loopSleep",
-                1,
-                "spin",
-                label="Service loop sleep (s)",
-                min=1,
-                max=60,
-            )
+            self._configuration.add("service.loopSleep", 1, "number", label="Service loop sleep (s)", min=1, max=60)
             # Default interval used when registering tasks (seconds)
-            self._configuration.add(
-                "service.defaultInterval",
-                60,
-                "spin",
-                label="Service default task interval (s)",
-                min=1,
-                max=86400,
-            )
+            self._configuration.add("service.defaultInterval", 0, "number", label="Service default task interval (s)", min=0, max=86400)
             # Persist any new defaults
             self._configuration.save()
 
