fa8b9f519c1b133601744a27504f6d262a94721f
Louis Ouellet <louis_ouellet@hotmail.com>
2026-01-15 12:26:54 -0500

    BUGFIX: Fixing the python namespace

--- database/__init__.py
+++ database/__init__.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 # src/core/filesystem/__init__.py
 
-from .database import SQLite
+from .sqlite import SQLite
 
 __version__ = "1.0.0"
 
-__all__ = ["FileSystem"]
+__all__ = ["SQLite"]
