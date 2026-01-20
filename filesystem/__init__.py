ae111905945666c9023fff2baf309031baba8171
Louis Ouellet <louis_ouellet@hotmail.com>
2026-01-20 09:44:17 -0500

    General: Updated the namespace.

--- filesystem/__init__.py
+++ filesystem/__init__.py
@@ -2,7 +2,8 @@
 # src/core/filesystem/__init__.py
 
 from .filesystem import FileSystem
+from .share import Share
 
 __version__ = "1.0.0"
 
-__all__ = ["FileSystem"]
+__all__ = ["FileSystem", "Share"]
